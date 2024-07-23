1caf0a660d262cffc4cea834434cff989a4fd557
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-23 15:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ let startGame =()=>{
 	mainBox.style.display = "flex"
   twoPlayers = [player1, player2];
   const endMessage = document.getElementById("message");
-  endMessage.textContent=`${twoPlayers[0]}, you're up`;
+  endMessage.textContent=`Player1, you're up`;
   for(let i=0;i<box.length;i++){
     box[i].addEventListener('click',()=>{
       if(box[i].textContent !== ""){
@@ -56,13 +56,9 @@ let startGame =()=>{
       console.log("checkWinner:", checkWinner(currentPlayer))
       
       if(checkWinner(currentPlayer)){
-        
-        let username = "";
-        if(currentPlayer == "X"){
-          username = twoPlayers[0]
-        }else{
-          username = twoPlayers[1]
-        }
+                // let username = currentPlayer == "X" ? twoPlayers[0]:twoPlayers[1];
+
+        let username = currentPlayer == "X" ? "Player1":"Player2";
         endMessage.textContent=`${username}, congratulations you won!`;
         return
       }
@@ -75,10 +71,15 @@ let startGame =()=>{
       
       currentPlayer = (currentPlayer ===  players[0]) ? players[1]:players[0];
       console.log("curr:", currentPlayer)
-      if(currentPlayer == players[0]){
-        endMessage.textContent=`${twoPlayers[0]}, you're up`
+      // if(currentPlayer == players[0]){
+      //   endMessage.textContent=`${twoPlayers[0]}, you're up`
+      // }else{ 
+      //   endMessage.textContent = `${twoPlayers[1]}, you're up`
+      // }
+		      if(currentPlayer == players[0]){
+        endMessage.textContent=`Player1, you're up`
       }else{ 
-        endMessage.textContent = `${twoPlayers[1]}, you're up`
+        endMessage.textContent = `Player2, you're up`
       }
     })
   }
